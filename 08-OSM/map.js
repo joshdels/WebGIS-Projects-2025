c846addbed38cda2ea360c03e22e67b1e512c7bf
joshdels <deleonjoshy2@gmail.com>
2025-11-19 01:11:53 +0800

    proj: finish day 14 OSM

--- 08-OSM/map.js
+++ 08-OSM/map.js
@@ -1,3 +1,18 @@
+const form = document.getElementById('locationForm');
+const input = document.getElementById('locationInput');
+const city = document.getElementById('city')
+const numberOfRestaurant = document.getElementById('restaurant');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let value = input.value
+  value = value.charAt(0).toUpperCase() + value.slice(1);
+  city.innerHTML = `Restaurants in ${value}`
+  getData(value);
+
+})
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -5,43 +20,78 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-async function getData() {
-  const url = 'gadm41_PHL_1.json'
+let currentLayer = null;
+
+async function getData(location) {
+  const url = "https://overpass-api.de/api/interpreter"
+
+  const query = `
+    [out:json][timeout:25];
+    area["name"="${location}"]->.searchArea;
+    node["amenity"="restaurant"](area.searchArea);
+    out;
+  `;
   
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: "POST",
+      body: query,
+    });
     if (!response.ok) {
       throw new Error(`Error fetching ${response.status}`);
     }
     const result = await response.json();
     console.log(result);
+    numberOfRestaurant.innerHTML = 
+      result.elements.length > 0
+      ? `Current Number: ${result.elements.length}` 
+      : `No restaurants found`;
+
+    const geo = {
+      type: "FeatureCollection",
+      features: result.elements.map(n => ({
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [n.lon, n.lat],
+        },
+        properties: n.tags
+      }))
+    }
+
+    if (currentLayer) {
+      map.removeLayer(currentLayer);
+    }
 
     let selectedLayer = null;
     
-    const layer = L.geoJSON(result, {
-      style: feature => ({ color: 'gray', weight: 2 }),
+    currentLayer = L.geoJSON(geo, {
+      pointToLayer: (feature, latlng) =>
+        L.circleMarker(latlng, {radius: 5, color: "blue"}),
+      
       onEachFeature: function(feature, layer) {
         layer.on('click', function(e) {
           if (selectedLayer && selectedLayer !== layer) {
-            selectedLayer.setStyle({ color: 'gray', weight: 2})
+            selectedLayer.setStyle({ color: 'blue'})
           }
           const isSelected = layer.options.color == "red";
-          layer.setStyle({ color: isSelected ? 'gray' : 'red', weight: 3})
+          layer.setStyle({ color: isSelected ? 'blue' : 'red'})
 
           selectedLayer = isSelected ? null : layer;
           
-          console.log(feature.properties.NAME_1);
-          layer.bindPopup(`${feature.properties.NAME_1}`).openPopup();
+          console.log(feature.properties.name);
+          layer.bindPopup(`${feature.properties.name}`).openPopup();
         })
       }
     }).addTo(map);
 
-    map.fitBounds(layer.getBounds());
+    if (geo.features.length > 0) {
+      map.fitBounds(currentLayer.getBounds());
+    }
 
   } catch (error) {
     console.error(error.message);
   }
 }
 
-getData(map);
 
